a81da59683ac95830125d974a719c84be3d74579
Jeevan Gyawali <jeewangw9@gmail.com>
2020-02-22 03:10:21 -0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -163,17 +165,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-  File _selectedFile;
+  File _selectedFile;         //  Image
   bool _inProcess = false;
-  Position _currentPosition;
-  String _currentAddress;
+  static double latitude;
+  static double longitude;
+   Position _currentPosition;
+
+   int show = 0;
+   
+  static String _currentAddress;
 
   Widget getImageWidget() {
     if (_selectedFile != null) {
       String currentPos = _currentPosition.toString();
       return Container (
         child: Column (
-          children: [ Image.file(
+          children: [ Image.file(       //Image File
         _selectedFile,
         width: 250,
         height: 250,
@@ -204,6 +211,8 @@ class _MyHomePageState extends State<MyHomePage> {
         .then((Position position) {
       setState(() {
         _currentPosition = position;
+        longitude = position.longitude;
+        latitude = position.latitude;
       });
       _getAddressFromLatLng();
     }).catchError((e) {
@@ -229,6 +238,7 @@ class _MyHomePageState extends State<MyHomePage> {
   getImage(ImageSource source) async {
       this.setState((){
         _inProcess = true;
+        show = 1;
       });
       File image = await ImagePicker.pickImage(source: source);
       if(image != null){
@@ -253,12 +263,65 @@ class _MyHomePageState extends State<MyHomePage> {
           _selectedFile = cropped;
           _inProcess = false;
         });
+
+        //FORM 
+
+        _showDialog();
+
       } else {
         this.setState((){
           _inProcess = false;
         });
       }
   }
+
+   void _showModalSheet() {      //SHOW MODAL SHEET Warning Text
+    showModalBottomSheet(context: context, builder: (builder) {
+      return Container(       
+        child: Text ("No image is uploaded to track"),
+        padding: EdgeInsets.all(40.0),
+      );
+    });
+  }
+
+  // Report Form
+  void _showDialog() {
+    showModalBottomSheet(isDismissible: false,context: context, builder: (builder) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Title',
+                ),
+              ),
+    TextField(
+  obscureText: false,
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    labelText: 'Description',
+  ),
+),
+const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () { 
+            Navigator.pop(context);
+          },
+          child: const Text(
+            'Submit',
+            style: TextStyle(fontSize: 10, color: Colors.blue)
+          ),
+        ),
+
+      ],
+    );
+    });
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -281,14 +344,35 @@ class _MyHomePageState extends State<MyHomePage> {
                       onPressed: () {
                         getImage(ImageSource.camera);
                       }),
-                  MaterialButton(
+                      MaterialButton(
+                      color: Colors.blue,
+                      child: Text(
+                        "Track in Map",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        if (show == 1) {
+
+                       Navigator.push(
+                      context,
+                       MaterialPageRoute(builder: (context) => new MapSample()
+                        ));
+                        }
+                        else {
+                          _showModalSheet();
+                          print ('No Picture is clicked!!');
+                        }
+
+                        }
+                      ),
+                  MaterialButton(       
                       color: Colors.deepOrange,
                       child: Text(
                         "Device",
                         style: TextStyle(color: Colors.white),
                       ),
                       onPressed: () {
-                        getImage(ImageSource.gallery);
+                        getImage(ImageSource.gallery);     
                       })
                 ],
               )
@@ -304,5 +388,88 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       )
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+
+static double latty = _MyHomePageState.latitude;    // Latitude
+static double longy = _MyHomePageState.longitude;   // Longitude
+
+List <Marker> allMarkers = [];
+
+@override
+void initState(){
+  super.initState();
+  allMarkers.add(Marker(
+    markerId: MarkerId('myMarker'),
+    infoWindow: InfoWindow(
+      title: 'Photo was taken at this position',
+      snippet: 'Trash',
+    ),
+    
+    draggable: false,
+    onTap: () {  
+      _showModalSheet();                   //Marker On Tap
+      print('Marker Tapped');
+    },
+    position: LatLng(latty,longy)
+  ));
+}
+
+ void _showModalSheet() {      //SHOW MODAL SHEET
+    showModalBottomSheet(context: context, builder: (builder) {
+      return Container(       
+        child: Image.network('https://i.stack.imgur.com/ilEcY.png'), // Get Trash pic from database
+        padding: EdgeInsets.all(40.0),
+      );
+    });
+  }
+
+  Completer<GoogleMapController> _controller = Completer();
+  
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(latty, longy),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        markers: Set.from(allMarkers),
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          print ("lattitude is $latty");
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    print ("lattitude is $latty");
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
